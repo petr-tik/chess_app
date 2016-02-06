9b3f6007962b742d2916c78121809cba981424fe
petr-tik <tikilyaynen@gmail.com>
2016-02-06 18:20:15 +0000

    first version of create tournament page

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
 
 app = Flask(__name__)
+app.config.from_object('config')
+
 from app import views
 # import at the end, cos app.views needs to import the app variable
 
--- app/views.py
+++ app/views.py
@@ -1,6 +1,11 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
-@app.route('/index')
+def home():
+    return render_template('index.html')
+
+@app.route('/create_tournament')
 def index():
-    return "Hello World!"
+    return render_template('create_tournament.html', title='Home')
+
