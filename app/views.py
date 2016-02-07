c810e998b3c813a8b5f8003972f10722e02247f3
petr-tik <tikilyaynen@gmail.com>
2016-02-07 19:45:14 +0000

    forms created, views updated, trying to make it work with html

--- app/views.py
+++ app/views.py
@@ -1,11 +1,29 @@
-from flask import render_template
+from flask import render_template, request, flash, redirect, url_for
 from app import app
+from models import Player, Tournament, Game
+from forms import ChooseTournament, CreateTournament, AddPlayers, RoundResults
 
-@app.route('/')
+@app.route('/', methods = ['GET', 'POST'])
 def home():
-    return render_template('index.html')
+	form = ChooseTournament(request.form)
+	if form.validate_on_submit():
+		if 'create' in request.form:
+			return redirect(url_for('create_tournament'))
+		elif 'load' in request.form:
+			return redirect(url_for('load_tournament'))
+	
+	return render_template('index.html')
 
-@app.route('/create_tournament')
-def index():
-    return render_template('create_tournament.html', title='Home')
+
+
+
+@app.route('/create_tournament', methods = ['GET','POST'])
+def create():
+    #form = CreateTournament(request.form)
+    #if request.method == 'POST' and form.validate():
+    	u	
+
+
+    #	return 
+	return render_template('create_tournament.html', title='Home')
 
---
+++ forms.py
@@ -0,0 +1,26 @@
+from flask_wtf import Form
+from datetime import date
+from wtforms import StringField, SelectField, validators, SubmitField, HiddenField, DateTimeField
+
+
+class ChooseTournament(Form):
+	which = SelectField('New tournament', choices=[('load old', 'load old'), ('create new', 'create new')])
+
+class CreateTournament(Form):
+	tourn_name = StringField('Tournament name', [validators.Length(min=4, max=25)])
+	system = SelectField('Tournament system', choices=[('Swiss', 'Swiss'), ('Round robin', 'Round robin')])
+	tie_break = SelectField('Tie breaker', choices=[('Swiss', 'Swiss'), ('Round robin', 'Round robin')])
+	location = StringField('Location')
+	date = DateTimeField(format='%d/%m/%Y', default="{:%d/%m/%Y}".format(date.today()))
+	submit = SubmitField("Create tournament")
+
+
+class AddPlayers(Form):
+	name = StringField('Name', [validators.Length(min=4)])
+	email = StringField('Email', [validators.Email(message="Insert valid email")])
+	submit = SubmitField("Start playing")
+
+
+class RoundResults(Form):
+	# for x in Games:
+	outcome = SelectField(choices=[('W','1-0'), ('D', 'Draw'), ('L', '0-1')])
