3f9fd650c078f6d5cd8df6ab0678cec5fe282192
petr-tik <tikilyaynen@gmail.com>
2016-02-18 12:48:00 +0000

    broke planner down into generate, robin_odd and robin_even, where robin_odd uses robin_even

--- src/planner.py
+++ src/planner.py
@@ -30,41 +30,59 @@ Thanks to @DRMacIver
 		else:
 			return True
 
-	def robin(self):
+	def robin_even(self):
+		"""
+		Thanks to @eterevsky for reading me rambling on about this, so I could start going the right direction
+
+		"""
+
 		# outputs a list of named tuples 
 		TOURNAMENT = []
 		# number of rounds = number of players - 1 - so everyone plays every other player
 		for x in xrange(len(self.players) - 1):
 			players = self.players
 			matches = []
-			while (len(players) > 1):
+			# constantly take the first member of the list
+			first = 0
 
-
-			else:
-				if len(players) == 0:
-					bye = 0
+			while players:
+				#make a pair
+				other = 1
+				potential_pair = (players[first], players[other])
+				if any(potential_pair in mat for mat in TOURNAMENT):
+					other += 1
+					continue
 				else:
-					bye = players[0]
-
-
-			TOURNAMENT.append(self.Round(matches,bye))
-
+					# append them to matches for the round
+					matches.append(potential_pair)
+					#delete them from players
+					players = [pl for pl in players if pl not in potential_pair]
+					continue
+				
+			bye = []
+			TOURNAMENT.append(matches)
+			print TOURNAMENT[x]
 		print TOURNAMENT
 
 
+	def robin_odd(self):
+		pass
+
+	def generate(self):
+		if self.system == 'robin':
+			if self.bye():
+				self.robin_odd()
+			else:
+				self.robin_even()
+		else:
+			pass
 
 
 
 
-"""
->>> Round = namedtuple('Round', ('matches', 'bye'))
->>> x = Round(["stuff"], "someone")
->>> x
-Round(matches=['stuff'], bye='someone')
->>> x.matches
-['stuff']
->>> x.bye
-'someone'
-"""
+even_lads = ['bob', 'john', 'max', 'adam']
+tourn = Tournament(even_lads, 'robin')
+tourn.generate()
+
 
 
