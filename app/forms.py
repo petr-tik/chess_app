c775c3687b6c066cb0a1d8cf91679ed960dd4a8a
petr-tik <tikilyaynen@gmail.com>
2016-02-25 22:10:16 +0000

    rewriting the templates according to forms. Need to pass custom html tags according to value

--- app/forms.py
+++ app/forms.py
@@ -11,16 +11,14 @@ class CreateTournament(Form):
 	tourn_name = StringField('Tournament name', [validators.Length(min=4, max=25)])
 	location = StringField('Location')
 	date = DateField(format='%d/%m/%Y')
-	system = SelectField('Tournament system', choices=[('swiss', 'Swiss system'), ('round_robin', 'Round robin')], render_kw={"label":"System"})
+	system = SelectField('Tournament system', choices=[('def', 'Tournament system'), ('swiss', 'Swiss system'), ('round_robin', 'Round robin')])
 	tie_break = SelectField('Tie breaker', choices=[('head', 'Head to head'), ('black', 'Wins with black')])
 	submit = SubmitField("Let's add players")
-
 
 class AddPlayers(Form):
 	name = StringField('Name', [validators.Length(min=4)])
 	email = StringField('Email', [validators.Email(message="Insert valid email")])
 	submit = SubmitField("Start playing")
-
 
 class RoundResults(Form):
 	# for x in Games:
