5a3db6d0982bc588e9d45dc88321f63423e08bed
petr-tik <tikilyaynen@gmail.com>
2016-02-27 10:25:02 +0000

    updated doc string and minor change in _BYE

--- src/planner.py
+++ src/planner.py
@@ -4,17 +4,24 @@ class GamePlan(object):
 	"""
 initialise the tournament object with an overall list of players and the system definition (swiss or robin)
 
-input that:
-
+input:
+a list of players
 
 output:
-a list of named tuples for each round
+a list (len = number of rounds) of lists of tuples 
+with players' names (maybe change to IDs from db) in white, black order
+
+GamePlans with odd number of players have each person sitting out
+Created as a tuple with ('_BYE', 'real player')
+Template needs to check for '_BYE' in each tuple and 
+
+
 where each tuple is of the form 
 (['people', 'playing'], 'person sitting out')
 Thanks to @DRMacIver
 
-
 """
+
 	def __init__(self, PLAYERS):
 		self.players = PLAYERS
 
@@ -43,7 +50,7 @@ Thanks to @DRMacIver
 			return self.berger_robin(players)	
 		else:
 			players = self.players
-			players.append('BYE')
+			players.append('_BYE')
 			return self.berger_robin(players)
 
 
