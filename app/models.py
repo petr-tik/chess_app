212215ea61621f7d5e247e916fbea8cfb06e90e3
petr-tik <tikilyaynen@gmail.com>
2016-02-28 17:57:00 +0000

    changed Models and created a new database successfully

--- app/models.py
+++ app/models.py
@@ -1,25 +1,20 @@
 from app import db
+"""
+create a tournament and make a database entry, save tournament ID in cookies session.
+add players from the table once it's been submitted. 
 
-class Player(db.Model):
-    ID = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(64), index=True, unique=True)
-    email = db.Column(db.String(120), index=True, unique=True)
-
-    def __init__(self, name, email):
-        self.name = name
-        self.email = email
-
-    def __repr__(self):
-        return '<Player {}>'.format(self.name)
+For round robin, make a schedule of games,
+"""
 
 class Tournament(db.Model):
-    ID = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(64))
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(32))
     location = db.Column(db.String(32))
     date = db.Column(db.DateTime)
-    system = db.Column(db.String(32)) # roundrobin or swiss
-    tie_break = db.Column(db.String(32))  # num won as black or head to head
-    
+    system = db.Column(db.String(10)) # roundrobin or swiss
+    tie_break = db.Column(db.String(10))  # num won as black or head to head
+    games = db.relationship('Game', backref = 'tournament', lazy = 'dynamic')
+
     def __init__(self, name, location, date, system, tie_break):
         self.name = name
         self.location = location
@@ -30,13 +25,35 @@ class Tournament(db.Model):
     def __repr__(self):
         return '<Tournament {}>'.format(self.name)    
 
+class Player(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(32), index=True, unique=True)
+    email = db.Column(db.String(64), index=True, unique=True)
+
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+
+    def __repr__(self):
+        return '<Player {}>'.format(self.name)
+
 class Game(db.Model):
-    ID = db.Column(db.Integer, primary_key=True)
-    TournamentID = db.Column(db.Integer) # pull in from tournament database
+    id = db.Column(db.Integer, primary_key=True)
+    # pull in from tournament database
+    TournamentID = db.Column(db.Integer, db.ForeignKey('tournament.id')) 
     round_num = db.Column(db.Integer)
-    white_id = db.Column(db.Integer)
-    black_id = db.Column(db.Integer) # pull players' IDs from Player db
-    outcome = db.Column(db.String(10)) # 'W', 'D' or 'L' white reference
+    # pull players' IDs from Player db
+    white_id = db.Column(db.Integer, db.ForeignKey('player.id'))
+    black_id = db.Column(db.Integer, db.ForeignKey('player.id')) 
+    outcome = db.Column(db.String(5)) # 'W', 'D' or 'L' white reference
+
+    def __init__(self, TournamentID, round_num, white_id, black_id, outcome=None):
+        self.TournamentID = TournamentID # pull in from Tournament info
+        self.round_num = round_num
+        self.white_id = white_id
+        self.black_id = black_id
+        if outcome:
+            self.outcome = outcome
 
     def __repr__(self):
         return '<Game {}>'.format(self.ID)    
