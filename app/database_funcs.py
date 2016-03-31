93f6cb11fc1911d836fed5fc09caf83a9608bc43
petr-tik <tikilyaynen@gmail.com>
2016-03-31 22:41:52 +0100

    working on a custom wrapper to check if round is last

--- app/database_funcs.py
+++ app/database_funcs.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+from functools import wraps
+
 # DATABASE INTERACTION FUNCTIONS
 
 def get_db():
@@ -26,3 +28,23 @@ def connect_db():
     rv = sqlite3.connect(app.config['DATABASE'])
     rv.row_factory = sqlite3.Row
     return rv
+
+# Wrapper func to return different last page of standings
+
+def is_last_round(func):
+    def decorator(f):
+        @wraps(func)
+        def decorated_function(*args, *kwargs):
+            # get the max round value and check if round_num is last round
+
+            if round_num == final_round:
+                return render_template('final_results.html')
+            else:
+                return render_template('standings.html', round_num=round_num)
+
+
+
+        return decorated_function
+    return decorator
+
+
