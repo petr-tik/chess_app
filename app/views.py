ff746da415f717cfb1b249976e522dab4178c7d5
petr-tik <tikilyaynen@gmail.com>
2016-04-02 20:35:40 +0100

    defined before and after request functions to open, commit and close db connections

--- app/views.py
+++ app/views.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 from flask import render_template, request, flash, redirect, url_for, g
 from app import app, db
 from models import Player, Tournament, Game
@@ -7,40 +8,46 @@ import sys
 import sqlite3
 from functools import wraps
 
+
 DATABASE = 'test.db'
+
+@app.before_request
+def before_request():
+    g.db = sqlite3.connect(DATABASE)
+
+@app.teardown_request
+def teardown_request(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.commit()
+        db.close()
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
-	conn = sqlite3.connect(DATABASE)
-	TOURNS = conn.execute('SELECT * FROM tournament').fetchall()
-	if request.method == 'POST':
+    TOURNS = g.db.execute('SELECT * FROM tournament').fetchall()
+    if request.method == 'POST':
 		if request.form['choice'] == 'Create new':	
 			return redirect(url_for("create_tournament"))
-			db.close()
+
 		elif request.form['choice'] == 'Load old':
 			# go to load pages and pull in all tournament entries from the database
 			return redirect(url_for("load_tournament"))
-	return render_template('index.html', tourns=TOURNS, title="Main menu")
+    
+    return render_template('index.html', tourns=TOURNS, title="Main menu")
 
 
 @app.route('/load_tournament', methods = ['GET', 'POST'])
 def load_tournament():
-	# connect to database and list all tournaments by name
-	conn = sqlite3.connect(DATABASE)
-	TOURNS = conn.execute('SELECT * FROM tournament').fetchall()
-	if request.method == 'POST':
-		tourn_to_load = request.form['tourn'] # selected tournament
+    if request.method == 'POST':
+        tourn_to_load = request.form['tourn'] # selected tournament
+    	return redirect(url_for("add_players"))
 
-		return redirect(url_for("add_players"))
-
-	return render_template('load_tournament.html', tourns=TOURNS)
-	# submit button load 
+    return render_template('load_tournament.html', tourns=TOURNS)
 
 
 @app.route('/create_tournament', methods = ['GET','POST'])
 def create_tournament():
 	# page 3
-    conn = sqlite3.connect(DATABASE)
     form = CreateTournament(request.form)
     if request.method == 'POST':
     	nam = request.form['name']
@@ -48,10 +55,8 @@ def create_tournament():
     	cal = request.form['calendar']
     	sys = request.form['system']
     	tie = request.form['tie_break']
-    	conn.execute('''INSERT INTO tournament (id, name, location, calendar, system, tie_break) \
+    	g.db.execute('''INSERT INTO tournament (id, name, location, calendar, system, tie_break) \
     		VALUES (NULL, ?, ?, ?, ?, ?)''', (nam, loc, cal, sys, tie))
-    	conn.commit()
-    	conn.close()
     	return redirect(url_for('add_players'))
 
     return render_template('create_tournament.html', title='Create new tournament', form=form)
@@ -62,16 +67,10 @@ def create_tournament():
 def add_players():
 	form = AddPlayers(request.form)
 	if request.method == 'POST':
-		if request.form['send'] == 'Start playing':
-			conn = sqlite3.connect(DATABASE)
-    		for player in PLAYERS:
-    			conn.execute('''INSERT INTO player (id, name, email) \ 
-    				VALUES (NULL, ?, ?)''', (player[0], player[1]))
-
-    		conn.commit()
-    		conn.close()
-		return redirect(url_for("round"))
-			# else:
+		pass
+		
+        return redirect(url_for("round"))
+    		# else:
 
 	return render_template('add_players.html',form=form)
 
@@ -80,20 +79,16 @@ def add_players():
 #def round(tournamentID, round_num):
 @app.route('/round', methods = ['GET', 'POST'])
 def round():
-	"""
+    """ 
     gets the round number, PLAYERS' names and round match schedule, 
-    pass it into the rendered template 
-    """
-    conn = sqlite3.connect(DATABASE)
+    pass it into the rendered template """
     cur = conn.cursor()
     games = cur.execute("select * from game where \
             game_tournament.tournament_id = ? \
             game.round = ?", tournamentID, round_num).fetchall()
-	
-	if request.method == 'POST':
+    if request.method == 'POST':
 		return redirect(url_for("standings"))
-
-	return render_template('round.html', round_num=round_num, NUM_GAMES=NUM_GAMES, players=players)
+    return render_template('round.html', round_num=round_num, NUM_GAMES=NUM_GAMES, players=players)
     	
 
 #@app.route('/<tournamentID>/<round_num>/standings', methods = ['GET', 'POST'])
@@ -102,8 +97,7 @@ def round():
 @app.route('/standings', methods = ['GET', 'POST'])
 def standings():
 	round_num = 4 # take from the database
-	if request.method == 'POST':
-        pass
-	return render_template('standings.html', round_num=round_num, PLAYERS=PLAYERS)
+	if request.method == 'POST': 
+	   return render_template('standings.html', round_num=round_num, PLAYERS=PLAYERS)
 
 
