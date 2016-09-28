e5c1cf7f0de30671e95150d455f1f69c02213e70
petr-tik <petr-tik@users.noreply.github.com>
2016-09-28 20:15:12 +0100

    pep8 compatible

--- src/planner.py
+++ src/planner.py
@@ -1,60 +1,56 @@
 from collections import deque
 
+
 class GamePlan(object):
-	"""
-initialise the tournament object with an overall list of players' IDs
 
-input:
-a list of players
+    """
+    initialise the tournament object with an overall list of players' IDs
 
-output:
-a list (len = number of rounds) of lists of tuples 
-with players' names (maybe change to IDs from db) in white, black order
+    input:
+    a list of players
 
-GamePlans with odd number of players have each person sitting out
-Created as a tuple with ('_BYE', 'real player')
-Template needs to check for '_BYE' in each tuple and 
+    output:
+    a list (len = number of rounds) of lists of tuples
+    with players' names (maybe change to IDs from db) in white, black order
 
+    GamePlans with odd number of players have each person sitting out
+    Created as a tuple with ('_BYE', 'real player')
+    Template needs to check for '_BYE' in each tuple and
+    """
 
-where each tuple is of the form 
-(['people', 'playing'], 'person sitting out')
-Thanks to @DRMacIver
+    def __init__(self, PLAYERS):
+        self.players = PLAYERS
 
-"""
+    def berger_robin(self, players):
+    # taken from
+    # https://en.wikipedia.org/wiki/Round-robin_tournament#Scheduling_algorithm
+        n = len(players)
+        shift = n / 2
+        last = players.pop()
+        pl_deque = deque(players)
+        TOURNAMENT = []
+        for x in xrange(n - 1):
+            round_dict = {'matches': [], 'bye': "__NONE"}
+            if last == '_BYE':
+                round_dict['bye'] = pl_deque[0]
+            else:
+                if x % 2 == 0:
+                    round_dict['matches'].append((last, pl_deque[0]))
+                else:
+                    round_dict['matches'].append((pl_deque[0], last))
+            other_games = [(pl_deque[x], pl_deque[x + 1])
+                           for x in xrange(1, (len(pl_deque) - 1), 2)]
+            pl_deque.rotate(shift)
+            round_dict['matches'] += other_games
+            TOURNAMENT.append(round_dict)
 
-	def __init__(self, PLAYERS):
-		self.players = PLAYERS
+        return TOURNAMENT
 
-	def berger_robin(self, players):
-        # taken from https://en.wikipedia.org/wiki/Round-robin_tournament#Scheduling_algorithm
-		n = len(players)
-		shift = n/2
-		last = players.pop()
-		pl_deque = deque(players)
-		TOURNAMENT = []
-		for x in xrange(n-1):
-                        round_dict = {'matches' : [], 'bye': "__NONE"}
-                        if last == '_BYE':
-                                round_dict['bye'] = pl_deque[0]
-                        else:
-                                if x % 2 == 0:
-                                        round_dict['matches'].append((last, pl_deque[0]))
-                                else:
-                                        round_dict['matches'].append((pl_deque[0], last))
-                        other_games = [(pl_deque[x], pl_deque[x+1]) for x in xrange(1,(len(pl_deque)-1), 2)]	
-                        pl_deque.rotate(shift)
-	                round_dict['matches'] += other_games
-                        TOURNAMENT.append(round_dict)
+    def generate(self):
+        if len(self.players) % 2 == 0:
+            players = self.players
+        else:
+            players = self.players
+            players.append('_BYE')
 
-		return TOURNAMENT
-
-	def generate(self):
-		if len(self.players) % 2 == 0:
-			players = self.players
-		else:
-			players = self.players
-			players.append('_BYE')
-
-                return self.berger_robin(players)
-
-		
+        return self.berger_robin(players)
