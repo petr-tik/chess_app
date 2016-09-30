7723ece9baee0ce52b053bb93f3658478ddd5f31
petr-tik <petr-tik@users.noreply.github.com>
2016-09-30 23:58:43 +0100

    added comments for tests and tests

--- tests/test_planner.py
+++ tests/test_planner.py
@@ -8,7 +8,7 @@ class testTournament(unittest.TestCase):
         self.players_odd = [
             'john',
             'bob',
-            'ana',
+            'generic_name',
             'kate',
             'paolo',
             'peter',
@@ -20,7 +20,7 @@ class testTournament(unittest.TestCase):
         self.players_even = [
             'john',
             'bob',
-            'ana',
+            'generic_name',
             'kate',
             'paolo',
             'peter',
@@ -33,21 +33,28 @@ class testTournament(unittest.TestCase):
         self.rounds_even = self.tourn_even_robin.generate()
         self.rounds_odd = self.tourn_odd_robin.generate()
 
+    def test_odd_is_odd(self):
+        self.assertTrue(len(self.players_odd) % 2 == 1)
+
+    def test_even_is_even(self):
+        self.assertTrue(len(self.players_even) % 2 == 0)
+
     def test_players_per_round(self):
-        # test that each round only has max number of players not more than
-        # double the number of couples
+        """test that each round only has max number of players not more than
+        double the number of couples"""
         for rnd in self.rounds_even:
             self.assertTrue(
                 len(self.tourn_even_robin.players) <= 2 * len(rnd['matches']))
 
     def test_everyone_plays_even(self):
-        # everyone must play in each round
+        """Test that everyone plays in each round with even player numbers"""
         for rnd in self.rounds_even:
             for player in self.players_even:
                 self.assertTrue(player in tup_rnd for tup_rnd in rnd)
 
     def test_everyone_plays_odd(self):
-        # everyone must play in each round
+        """everyone must play in each round in a tournament 
+        with odd number of players"""
         for rnd in self.rounds_odd:
             for player in self.players_odd:
                 self.assertTrue(player in tup_rnd for tup_rnd in rnd)
